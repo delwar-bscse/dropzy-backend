c9b14bb00511c892d70bfb8f1b1e384871d72042
Nadir Hossain <nadirhossain336@gmail.com>
2024-12-27 16:47:53 +0600

    refactor: :recycle: simplify ID validation logic in checkValidID function

--- src/shared/checkValidID.ts
+++ src/shared/checkValidID.ts
@@ -2,12 +2,9 @@ import { z } from "zod";
 import mongoose from "mongoose";
 
 export const checkValidID = (value: string) =>
-    z.string({ required_error: `${value} is required` }).refine((val) => {
-        
+    z.string({ required_error: `${value}` }).refine((val) => {
         const isValidObjectId = mongoose.Types.ObjectId.isValid(val);
-        const matchesPattern = /^[a-fA-F0-9]{24}$/.test(val);
-        return isValidObjectId && matchesPattern;
-        
+        return isValidObjectId;
     }, {
         message: `Invalid ${value} format`,
     });
