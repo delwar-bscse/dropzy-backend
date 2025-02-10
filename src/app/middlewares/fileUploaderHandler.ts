fbdb3505cd9a40574d34a07f0fa4e7f013c293fe
Nadir Hossain <nadirhossain336@gmail.com>
2025-02-10 12:21:38 +0600

    refactor: :recycle: streamline file upload handler by centralizing upload directory management

--- src/app/middlewares/fileUploaderHandler.ts
+++ src/app/middlewares/fileUploaderHandler.ts
@@ -5,6 +5,11 @@ import multer, { FileFilterCallback } from 'multer';
 import path from 'path';
 import ApiError from '../../errors/ApiErrors';
 
+const uploadDirectories: Record<string, string> = {
+    image: 'image'
+};
+
+
 const fileUploadHandler = () => {
 
     //create upload folder
@@ -16,7 +21,7 @@ const fileUploadHandler = () => {
     //folder create for different file
     const createDir = (dirPath: string) => {
         if (!fs.existsSync(dirPath)) {
-        fs.mkdirSync(dirPath);
+            fs.mkdirSync(dirPath);
         }
     };
 
@@ -24,14 +29,13 @@ const fileUploadHandler = () => {
     const storage = multer.diskStorage({
 
         destination: (req, file, cb) => {
-            let uploadDir;
-            switch (file.fieldname) {
-                case 'image':
-                    uploadDir = path.join(baseUploadDir, 'images');
-                break;
-                default:
-                    throw new ApiError(StatusCodes.BAD_REQUEST, 'File is not supported');
+            const folderName = uploadDirectories[file.fieldname];
+
+            if (!folderName) {
+                cb(new ApiError(StatusCodes.BAD_REQUEST, 'File type not supported'), '');
             }
+
+            const uploadDir = path.join(baseUploadDir, folderName);
             createDir(uploadDir);
             cb(null, uploadDir);
         },
@@ -40,10 +44,10 @@ const fileUploadHandler = () => {
             const fileExt = path.extname(file.originalname);
             const fileName =
                 file.originalname
-                .replace(fileExt, '')
-                .toLowerCase()
-                .split(' ')
-                .join('-') +
+                    .replace(fileExt, '')
+                    .toLowerCase()
+                    .split(' ')
+                    .join('-') +
                 '-' +
                 Date.now();
             cb(null, fileName + fileExt);
@@ -64,13 +68,13 @@ const fileUploadHandler = () => {
             } else {
                 cb(new ApiError(StatusCodes.BAD_REQUEST, 'Only .jpeg, .png, .jpg file supported'))
             }
-        }else {
+        } else {
             cb(new ApiError(StatusCodes.BAD_REQUEST, 'This file is not supported'))
         }
     };
 
-    const upload = multer({ storage: storage, fileFilter: filterFilter})
-    .fields([{ name: 'image', maxCount: 3 } ]);
+    const upload = multer({ storage: storage, fileFilter: filterFilter })
+        .fields([{ name: 'image', maxCount: 3 }]);
     return upload;
 
 };
