71dadf18a674e0e08b2f88b4f5f35f3b9c17175b
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-03-23 14:11:27 +0600

    comment cornJob temporarily

--- src/helpers/cornJob.ts
+++ src/helpers/cornJob.ts
@@ -1,15 +1,15 @@
-import cron from "node-cron";
-import { UserService } from "../app/modules/user/user.service";
+// import cron from "node-cron";
+// import { UserService } from "../app/modules/user/user.service";
 
 // Runs every minute
-cron.schedule("* * * * *", async () => {
-  console.log("Node Corn Working Every 1 minute");
-  try {
-    await UserService.deleteUnverifiedAccount();
-  } catch (err) {
-    console.error("❌ Error in cleanup job:", err);
-  }
-});
+// cron.schedule("* * * * *", async () => {
+//   console.log("Node Corn Working Every 1 minute");
+//   try {
+//     await UserService.deleteUnverifiedAccount();
+//   } catch (err) {
+//     console.error("❌ Error in cleanup job:", err);
+//   }
+// });
 
 // Runs every 10 minutes
 // cron.schedule("*/10 * * * *", async () => {
